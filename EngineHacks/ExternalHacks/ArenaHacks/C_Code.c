81d58db51a8050b29f63a16bac97766bd5c692db
Jopettajah <joonaleivo99@gmail.com>
2024-04-21 16:07:11 +0300

    more arena-abuse fixes

--- EngineHacks/ExternalHacks/ArenaHacks/C_Code.c
+++ EngineHacks/ExternalHacks/ArenaHacks/C_Code.c
@@ -4,6 +4,13 @@ extern int GetUnitBestWRankType(struct Unit*);
 extern struct PlaySt gPlaySt;
 extern struct ROMChapterData* GetChapterDefinition(int index);
 extern int ExtraArenaTextID_Link;
+extern struct ProcCmd gProcScr_ArenaUiResults[];
+extern void MainUpdate_8055C68();
+extern struct ProcCmd ProcScr_efxStatusUnit[];
+extern void OnGameLoopMain();
+extern void OnVBlank();
+extern struct LCDControlBuffer gLCDControlBuffer;
+extern u32 GetBattleMapKind(void);
 
 s8 ArenaIsUnitAllowed(struct Unit* unit) {
     if (unit->statusIndex == UNIT_STATUS_SILENCED) {
@@ -21,10 +28,14 @@ s8 ArenaIsUnitAllowed(struct Unit* unit) {
     return 1;
 }
 
-void ArenaBegin(struct Unit* unit) {
+void ExecBattleAnimArenaExit(void)
+{
     gPlaySt.unk19 += 1;
-    StoreRNState(&gActionData.item);
-    ArenaBeginInternal(unit);
+    AnimClearAll();
+    NewEkrTogiEndPROC();
+
+    SetMainUpdateRoutine(MainUpdate_8055C68);
+    Proc_EndEach(ProcScr_efxStatusUnit);
 
     return;
 }
@@ -52,3 +63,85 @@ u8 ArenaCommandEffect(struct MenuProc* menu, struct MenuItemProc* menuItem) {
     return MENU_ACT_SKIPCURSOR | MENU_ACT_END | MENU_ACT_SND6A | MENU_ACT_CLEAR;
 
 }
+/*
+void SetBlendConfig(u16 effect, u8 coeffA, u8 coeffB, u8 blendY)
+{
+    gLCDControlBuffer.bldcnt.effect = effect;
+    gLCDControlBuffer.blendCoeffA = coeffA;
+    gLCDControlBuffer.blendCoeffB = coeffB;
+    gLCDControlBuffer.blendY = blendY;
+}
+
+void StartBattleMap(struct GameCtrlProc* gameCtrl) {
+    int i;
+
+    SetupBackgrounds(NULL);
+
+    SetMainUpdateRoutine(OnGameLoopMain);
+    SetInterrupt_LCDVBlank(OnVBlank);
+
+    ClearBattleMapState();
+    sub_80156D4();
+    SetupMapSpritesPalettes();
+    ResetChapterFlags();
+    ResetUnitSprites();
+    ResetMenuOverrides();
+    ClearTraps();
+
+    gPlaySt.faction = FACTION_GREEN; // TODO: PHASE/ALLEGIANCE DEFINITIONS
+    gPlaySt.chapterTurnNumber = 0;
+
+    // TODO: BATTLE MAP/CHAPTER/OBJECTIVE TYPE DEFINITION (STORY/TOWER/SKIRMISH)
+    //if (GetBattleMapKind() == 2) {
+    if (!(gPlaySt.chapterStateBits & PLAY_FLAG_PREPSCREEN)) {
+        gPlaySt.chapterVisionRange = 3 * (NextRN_100() & 1);
+    }
+    else {
+        gPlaySt.chapterVisionRange =
+            GetChapterDefinition(gPlaySt.chapterIndex)->initialFogLevel;
+    }
+
+    gPlaySt.chapterWeatherId =
+        GetChapterDefinition(gPlaySt.chapterIndex)->initialWeather;
+
+    InitBmBgLayers();
+    InitChapterMap(gPlaySt.chapterIndex);
+    InitMapObstacles();
+
+    gPlaySt.time_chapter_started = GetGameClock();
+    gPlaySt.chapterTotalSupportGain = 0;
+
+    gPlaySt.unk48 = 0;
+    gPlaySt.unk4A_1 = 0;
+    gPlaySt.tutorial_counter = 0;
+    gPlaySt.tutorial_exec_type = 0;
+    gPlaySt.unk19 = 0;
+
+    for (i = 1; i < 0x40; ++i) {
+        struct Unit* unit = GetUnit(i);
+
+        if (unit && unit->pCharacterData) {
+            if (unit->state & US_BIT21)
+                unit->state = unit->state | US_NOT_DEPLOYED;
+            else
+                unit->state = unit->state & ~US_NOT_DEPLOYED;
+        }
+    }
+
+    ClearTemporaryUnits();
+    LoadChapterTraps();
+
+    if (gameCtrl)
+        StartBMapMain(gameCtrl);
+
+    // TODO: MACRO?
+    gPaletteBuffer[PAL_BACKDROP_OFFSET] = 0;
+    EnablePaletteSync();
+
+    SetBlendTargetA(TRUE, TRUE, TRUE, TRUE, TRUE);
+    SetBlendBackdropA(TRUE);
+
+    SetBlendConfig(3, 0, 0, 0x10);
+}
+
+*/
--- EngineHacks/ExternalHacks/ArenaHacks/C_Code.h
+++ EngineHacks/ExternalHacks/ArenaHacks/C_Code.h
@@ -381,6 +381,198 @@ enum
     MENU_ACT_DOOM = (1 << 7),
 };
 
+struct ChapterEventGroup
+{
+    /* 00 */ const void* turnBasedEvents;
+    /* 04 */ const void* characterBasedEvents; // must be 32-Aligned?
+    /* 08 */ const void* locationBasedEvents;
+    /* 0C */ const void* miscBasedEvents;
+
+    // select unit -> select destination -> move unit to destination
+    /* 10 */ const void* specialEventsWhenUnitSelected;
+    /* 14 */ const void* specialEventsWhenDestSelected;
+    /* 18 */ const void* specialEventsAfterUnitMoved;
+
+    /* 1C */ const void* tutorialEvents;
+
+    /* 20 */ const void* traps;
+    /* 24 */ const void* extraTrapsInHard;
+
+    /* 28 */ const void* playerUnitsInNormal;
+    /* 2C */ const void* playerUnitsInHard;
+
+    /* 30 */ const void* playerUnitsChoice1InEncounter;
+    /* 34 */ const void* playerUnitsChoice2InEncounter;
+    /* 38 */ const void* playerUnitsChoice3InEncounter;
+
+    /* 3C */ const void* enemyUnitsChoice1InEncounter;
+    /* 40 */ const void* enemyUnitsChoice2InEncounter;
+    /* 44 */ const void* enemyUnitsChoice3InEncounter;
+
+    /* 48 */ const void* beginningSceneEvents;
+    /* 4C */ const void* endingSceneEvents;
+};
+
+
+struct ProcCmd
+{
+    short opcode;
+    short dataImm;
+    const void* dataPtr;
+};
+
+typedef void* ProcPtr;
+
+#define PROC_TREE_3     ((ProcPtr) 3)
+
+enum PlaySt_chapterStateBits {
+    PLAY_FLAG_STATSCREENPAGE0 = (1 << 0),
+    PLAY_FLAG_STATSCREENPAGE1 = (1 << 1),
+    PLAY_FLAG_POSTGAME = (1 << 2),
+    PLAY_FLAG_TUTORIAL = (1 << 3),
+    PLAY_FLAG_PREPSCREEN = (1 << 4),
+    PLAY_FLAG_COMPLETE = (1 << 5),
+    PLAY_FLAG_HARD = (1 << 6),
+    PLAY_FLAG_EXTRA_MAP = (1 << 7),
+
+    PLAY_FLAG_STATSCREENPAGE_SHIFT = 0,
+    PLAY_FLAG_STATSCREENPAGE_MASK = PLAY_FLAG_STATSCREENPAGE0 | PLAY_FLAG_STATSCREENPAGE1,
+};
+
+#define PAL_COLOR_OFFSET(palid, colornum) (palid) * 0x10 + (colornum)
+#define PAL_BACKDROP_OFFSET PAL_COLOR_OFFSET(0, 0)
+
+extern u16 gPaletteBuffer[];
+
+#if !defined(MODERN) || !MODERN
+#  define STRUCT_PAD(from, to) unsigned char _pad_ ## from[(to) - (from)]
+#else
+#  define STRUCT_PAD(from, to)
+#endif
+
+struct DispCnt {
+    /* bit  0 */ u16 mode : 3;
+    /* bit  3 */ u16 cgbMode : 1; // reserved, do not use
+    /* bit  4 */ u16 bmpFrameNum : 1;
+    /* bit  5 */ u16 hblankIntervalFree : 1;
+    /* bit  6 */ u16 obj1dMap : 1;
+    /* bit  7 */ u16 forcedBlank : 1;
+    /* bit  8 */ u16 bg0_on : 1;
+    /* bit  9 */ u16 bg1_on : 1;
+    /* bit 10 */ u16 bg2_on : 1;
+    /* bit 11 */ u16 bg3_on : 1;
+    /* bit 12 */ u16 obj_on : 1;
+    /* bit 13 */ u16 win0_on : 1;
+    /* bit 14 */ u16 win1_on : 1;
+    /* bit 15 */ u16 objWin_on : 1;
+    STRUCT_PAD(0x02, 0x04);
+};
+
+struct BgCnt {
+    /* bit  0 */ u16 priority : 2;
+    /* bit  2 */ u16 charBaseBlock : 2;
+    /* bit  4 */ u16 dummy : 2;
+    /* bit  6 */ u16 mosaic : 1;
+    /* bit  7 */ u16 colorMode : 1;
+    /* bit  8 */ u16 screenBaseBlock : 5;
+    /* bit 13 */ u16 areaOverflowMode : 1;
+    /* bit 14 */ u16 screenSize : 2;
+    STRUCT_PAD(0x02, 0x04);
+};
+
+struct WinCnt {
+    u8 win0_enableBg0 : 1;
+    u8 win0_enableBg1 : 1;
+    u8 win0_enableBg2 : 1;
+    u8 win0_enableBg3 : 1;
+    u8 win0_enableObj : 1;
+    u8 win0_enableBlend : 1;
+    u8 : 2;
+
+    u8 win1_enableBg0 : 1;
+    u8 win1_enableBg1 : 1;
+    u8 win1_enableBg2 : 1;
+    u8 win1_enableBg3 : 1;
+    u8 win1_enableObj : 1;
+    u8 win1_enableBlend : 1;
+    u8 : 2;
+
+    u8 wout_enableBg0 : 1;
+    u8 wout_enableBg1 : 1;
+    u8 wout_enableBg2 : 1;
+    u8 wout_enableBg3 : 1;
+    u8 wout_enableObj : 1;
+    u8 wout_enableBlend : 1;
+    u8 : 2;
+
+    u8 wobj_enableBg0 : 1;
+    u8 wobj_enableBg1 : 1;
+    u8 wobj_enableBg2 : 1;
+    u8 wobj_enableBg3 : 1;
+    u8 wobj_enableObj : 1;
+    u8 wobj_enableBlend : 1;
+    u8 : 2;
+};
+
+struct BlendCnt {
+    u16 target1_bg0_on : 1;
+    u16 target1_bg1_on : 1;
+    u16 target1_bg2_on : 1;
+    u16 target1_bg3_on : 1;
+    u16 target1_obj_on : 1;
+    u16 target1_bd_on : 1;
+    u16 effect : 2;
+    u16 target2_bg0_on : 1;
+    u16 target2_bg1_on : 1;
+    u16 target2_bg2_on : 1;
+    u16 target2_bg3_on : 1;
+    u16 target2_obj_on : 1;
+    u16 target2_bd_on : 1;
+    u32 _unused;
+};
+
+struct BgCoords
+{
+    u16 x;
+    u16 y;
+};
+
+struct LCDControlBuffer {
+    /* 00 */ struct DispCnt dispcnt;
+    /* 04 */ struct DispStat dispstat;
+    /* 08 */ STRUCT_PAD(0x08, 0x0C);
+    /* 0C */ struct BgCnt bg0cnt;
+    /* 10 */ struct BgCnt bg1cnt;
+    /* 14 */ struct BgCnt bg2cnt;
+    /* 18 */ struct BgCnt bg3cnt;
+    /* 1C */ struct BgCoords bgoffset[4];
+    /* 2C */ u8 win0_right, win0_left;
+    /* 2C */ u8 win1_right, win1_left;
+    /* 30 */ u8 win0_bottom, win0_top;
+    /* 30 */ u8 win1_bottom, win1_top;
+    /* 34 */ struct WinCnt wincnt;
+    /* 38 */ u16 mosaic;
+    STRUCT_PAD(0x3A, 0x3C);
+    /* 3C */ struct BlendCnt bldcnt;
+    /* 44 */ u8 blendCoeffA;
+    /* 45 */ u8 blendCoeffB;
+    /* 46 */ u8 blendY;
+    /* 48 */ u16 bg2pa;
+    /* 4A */ u16 bg2pb;
+    /* 4C */ u16 bg2pc;
+    /* 4E */ u16 bg2pd;
+    /* 50 */ u32 bg2x;
+    /* 54 */ u32 bg2y;
+    /* 58 */ u16 bg3pa;
+    /* 5A */ u16 bg3pb;
+    /* 5C */ u16 bg3pc;
+    /* 5E */ u16 bg3pd;
+    /* 60 */ u32 bg3x;
+    /* 64 */ u32 bg3y;
+    /* 68 */ s8 colorAddition;
+};
+
+
 
 
 
